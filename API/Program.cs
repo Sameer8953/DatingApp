fe9d9866a362f6624c3a4d178731671f3f0efbb9
sameerahmad <sameerahmad8878@outlook.com>
2023-01-28 20:46:50 +0530

    End of Lesson-3

--- API/Program.cs
+++ API/Program.cs
@@ -6,14 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<DataContext>(options => {
+builder.Services.AddDbContext<DataContext>(options =>
+{
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));    
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200") );
+
 app.MapControllers();
 
 app.Run();
